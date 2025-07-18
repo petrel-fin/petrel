9115e280df69c9632accc7f9320d12fe5c13e255
Chris Bellanti <ctbellanti@gmail.com>
2025-07-17 20:55:18 -0400

    Fixed imports

--- src/components/charts/sankey/sankey-chart.tsx
+++ src/components/charts/sankey/sankey-chart.tsx
@@ -1,62 +1,70 @@
-'use client'
+"use client";
 
-import { ResponsiveSankey } from '@nivo/sankey'
+import React from "react";
+import { Chart } from "react-google-charts";
 
-const data = {
-  nodes: [
-    { id: 'A' },
-    { id: 'B' },
-    { id: 'C' },
-    { id: 'D' },
-    { id: 'E' },
-    { id: 'F' },
-    { id: 'G' },
-    { id: 'H' },
-    { id: 'I' },
-  ],
-  links: [
-    { source: 'A', target: 'B', value: 10 },
-    { source: 'A', target: 'C', value: 5 },
-    { source: 'B', target: 'D', value: 7 },
-    { source: 'C', target: 'D', value: 3 },
-    { source: 'B', target: 'E', value: 2 },
-    { source: 'C', target: 'F', value: 2 },
-    { source: 'D', target: 'G', value: 6 },
-    { source: 'E', target: 'G', value: 1 },
-    { source: 'F', target: 'G', value: 1 },
-    { source: 'G', target: 'H', value: 7 },
-    { source: 'H', target: 'I', value: 6 },
-    { source: 'D', target: 'I', value: 1 },
-  ],
-}
+export const data = [
+  ["From", "To", "Weight"],
+  ["Brazil", "Portugal", 5],
+  ["Brazil", "France", 1],
+  ["Brazil", "Spain", 1],
+  ["Brazil", "England", 1],
+  ["Canada", "Portugal", 1],
+  ["Canada", "France", 5],
+  ["Canada", "England", 1],
+  ["Mexico", "Portugal", 1],
+  ["Mexico", "France", 1],
+  ["Mexico", "Spain", 5],
+  ["Mexico", "England", 1],
+  ["USA", "Portugal", 1],
+  ["USA", "France", 1],
+  ["USA", "Spain", 1],
+  ["USA", "England", 5],
+  ["Portugal", "Angola", 2],
+  ["Portugal", "Senegal", 1],
+  ["Portugal", "Morocco", 1],
+  ["Portugal", "South Africa", 3],
+  ["France", "Angola", 1],
+  ["France", "Senegal", 3],
+  ["France", "Mali", 3],
+  ["France", "Morocco", 3],
+  ["France", "South Africa", 1],
+  ["Spain", "Senegal", 1],
+  ["Spain", "Morocco", 3],
+  ["Spain", "South Africa", 1],
+  ["England", "Angola", 1],
+  ["England", "Senegal", 1],
+  ["England", "Morocco", 2],
+  ["England", "South Africa", 7],
+  ["South Africa", "China", 5],
+  ["South Africa", "India", 1],
+  ["South Africa", "Japan", 3],
+  ["Angola", "China", 5],
+  ["Angola", "India", 1],
+  ["Angola", "Japan", 3],
+  ["Senegal", "China", 5],
+  ["Senegal", "India", 1],
+  ["Senegal", "Japan", 3],
+  ["Mali", "China", 5],
+  ["Mali", "India", 1],
+  ["Mali", "Japan", 3],
+  ["Morocco", "China", 5],
+  ["Morocco", "India", 1],
+  ["Morocco", "Japan", 3],
+];
 
-const SankeyChart = () => {
+export const options = {};
+
+export default function SankeyChart() {
   return (
-    <div className='h-[500px] w-full'>
-      <ResponsiveSankey
+    <div className="flex h-full w-full p-4">
+      <Chart
+        chartType="Sankey"
+        width="100%"
+        height="500px"
         data={data}
-        margin={{ top: 40, right: 160, bottom: 40, left: 50 }}
-        align='justify'
-        colors={{ scheme: 'category10' }}
-        // nodeOpacity={1}
-        // nodeHoverOthersOpacity={0.35}
-        nodeThickness={18}
-        nodeSpacing={24}
-        nodeBorderWidth={1}
-        // nodeBorderColor={{ from: 'color', modifiers: [['darker', 0.8]] }}
-        // linkOpacity={0.5}
-        linkHoverOthersOpacity={0.1}
-        linkContract={0}
-        labelPosition='outside'
-        labelOrientation='horizontal'
-        labelPadding={16}
-        // labelTextColor={{ from: 'color', modifiers: [['darker', 1]] }}
-        enableLinkGradient={true}
-        animate={true}
-        motionConfig='gentle'
+        options={options}
       />
     </div>
-  )
+  );
 }
-
-export default SankeyChart
